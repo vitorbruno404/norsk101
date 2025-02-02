98de75f850f5bb1fdb7288215f5bbe170a8f12b6
vitorbruno404 <vitor.bruno@vitorbruno.com>
2025-02-02 11:27:01 +0100

    Remove GitHub link and descriptive text from demo page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ import SimliAgent from "@/app/SimliAgent";
 import DottedFace from "./Components/DottedFace";
 import SimliHeaderLogo from "./Components/Logo";
 import Navbar from "./Components/Navbar";
-import Image from "next/image";
-import GitHubLogo from "@/media/github-mark-white.svg";
 
 const Demo: React.FC = () => {
   const [showDottedFace, setShowDottedFace] = useState(true);
@@ -25,17 +23,6 @@ const Demo: React.FC = () => {
       <SimliHeaderLogo />
       <Navbar />
 
-      <div className="absolute top-[32px] right-[32px]">
-        <text
-          onClick={() => {
-            window.open("https://github.com/simliai/create-simli-agent");
-          }}
-          className="font-bold cursor-pointer mb-8 text-xl leading-8"
-        >
-          <Image className="w-[20px] inline mr-2" src={GitHubLogo} alt="" />
-          create-simli-agent
-        </text>
-      </div>
       <div className="flex flex-col items-center gap-6 bg-effect15White p-6 pb-[40px] rounded-xl w-full">
         <div>
           {showDottedFace && <DottedFace />}
@@ -47,26 +34,8 @@ const Demo: React.FC = () => {
       </div>
 
       <div className="max-w-[350px] font-thin flex flex-col items-center ">
-        <span className="font-bold mb-[8px] leading-5 ">
-          {" "}
-          Create Simli Agent is a starter repo for creating visual avatars with
-          Simli{" "}
-        </span>
         <ul className="list-decimal list-inside max-w-[350px] ml-[6px] mt-2">
-          <li className="mb-1">
-            Fill in your Simli API keys in .env file.
-          </li>
-          <li className="mb-1">
-            Create your Simli avatar at app.simli.com
-          </li>
-          <li className="mb-1">
-            Copy code output from app.simli.com and paste in <code>app/SimliAgent.tsx</code>.
-          </li>
         </ul>
-        <span className=" mt-[16px]">
-          You can now deploy this app to Vercel, or incorporate it as part of
-          your existing project.
-        </span>
       </div>
     </div>
   );
